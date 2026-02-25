3bb2bc9234379da93944b4584dd5a9e2cd631921
panagiotiskaragiannis <panoskarajohb@hotmail.com>
2026-02-25 18:19:01 +0000

    cleanup plus diagnostics off

--- lua/plugins/minimap.lua
+++ lua/plugins/minimap.lua
@@ -1,3 +1,10 @@
 return {
   "Isrothy/neominimap.nvim",
+  init = function()
+    vim.g.neominimap = vim.tbl_deep_extend("force", vim.g.neominimap or {}, {
+      diagnostic = {
+        enabled = false,
+      },
+    })
+  end,
 }
