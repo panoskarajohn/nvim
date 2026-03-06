5be08cd9305c04cd42947ea5971265de650fce0d
panagiotiskaragiannis <panoskarajohb@hotmail.com>
2026-03-06 19:48:48 +0000

    updated size of minimap

--- lua/plugins/minimap.lua
+++ lua/plugins/minimap.lua
@@ -2,6 +2,12 @@ return {
   "Isrothy/neominimap.nvim",
   init = function()
     vim.g.neominimap = vim.tbl_deep_extend("force", vim.g.neominimap or {}, {
+      float = {
+        minimap_width = 4,
+      },
+      split = {
+        minimap_width = 4,
+      },
       diagnostic = {
         enabled = false,
       },
